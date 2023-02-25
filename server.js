a5cdccfb43253021922235346b155a0ed99a60af
mangala-karunarathne <mangalaicc@gmail.com>
2023-02-25 07:04:06 +0530

    CHG | Create Folders and Home Route

--- server.js
+++ server.js
@@ -6,10 +6,20 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middlewares
+app.use(express.json());
+// app.use(cookieParser());  
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 
 // Connect to DB and start server 
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
