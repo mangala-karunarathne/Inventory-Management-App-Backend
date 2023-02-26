c9ba1f47da2e4edf28c72c32114ef92e6b8e2971
mangala-karunarathne <mangalaicc@gmail.com>
2023-02-26 16:30:23 +0530

    CHG | Create User Route

--- server.js
+++ server.js
@@ -3,22 +3,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-// app.use(cookieParser());  
+// app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Routes Middleware
+app.use("/api/users", userRoute)
+
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
-
-// Connect to DB and start server 
+// Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
