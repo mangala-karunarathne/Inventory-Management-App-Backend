7049e01d6f6bf62e01011c0c5450289f6c077d28
mangala-karunarathne <mangalaicc@gmail.com>
2023-03-05 11:48:46 +0530

    CHG | Register New User

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
@@ -14,14 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes Middleware
-app.use("/api/users", userRoute)
-
+app.use("/api/users", userRoute);
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
+// Error Middleware
+app.use(errorHandler);
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
