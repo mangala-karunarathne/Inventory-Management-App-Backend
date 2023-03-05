c042f57dd942d357c43bdc4760bcf47d764d83df
mangala-karunarathne <mangalaicc@gmail.com>
2023-03-05 18:18:23 +0530

    CHG | Encrypt Password Method 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const bcrypt = require("bcryptjs");
+
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -22,16 +22,12 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Email has alredy been registered");
   }
 
-  // Encrypt Password before saving to DB
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-
+ 
   // Create new User
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password,
   });
 
   if (user) {
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -44,6 +45,19 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Encrypt Password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
